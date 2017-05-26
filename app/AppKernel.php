60e2397d1223c362c39e64c362c317397c9f0807
thuanngo <nvt1020@gmail.com>
2017-05-26 22:55:43 +0700

    commit src env

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new Sf\AdminBundle\SfAdminBundle(),
+            new Sf\CoreBundle\SfCoreBundle(),
+            new Sf\FrontBundle\SfFrontBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
--- src/Sf/FrontBundle/SfFrontBundle.php
+++ src/Sf/FrontBundle/SfFrontBundle.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace AppBundle;
+namespace Sf\FrontBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class AppBundle extends Bundle
+class SfFrontBundle extends Bundle
 {
 }

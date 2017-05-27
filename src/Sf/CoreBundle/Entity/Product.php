a516a22d5a678ac1a528748af5451bf6de71314d
thuanngo <nvt1020@gmail.com>
2017-05-27 09:49:08 +0700

    cm src

---
+++ src/Sf/CoreBundle/Entity/Product.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Sf\CoreBundle\Entity;
+
+
+class Product
+{
+    private $id
+    ;
+    private $name;
+    private $price;
+    private $description;
+}
---
+++ src/Sf/CoreBundle/Entity/User.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Sf\CoreBundle\Entity;
+
+class User
+{
+    private $id;
+}
